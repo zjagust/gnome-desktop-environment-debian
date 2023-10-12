8b8ece1366e1feccbdacc196d90ad6a07fb0100d
Zeljko Jagust <zacks.adm@gmail.com>
2023-10-12 10:16:15 +0200

    Added rclone install

--- gnome-debian-autoinstall.sh
+++ gnome-debian-autoinstall.sh
@@ -99,14 +99,14 @@ function installGnome () {
 		DEBIAN_FRONTEND=noninteractive aptitude install -y -R alsa-utils chrome-gnome-shell cups-common dbus-x11 \
 		dconf-editor firefox-esr fuse_ fuse3 gdm3 gjs gkbd-capplet gnome-applets gnome-control-center gnome-disk-utility \
 		gnome-keyring gnome-session gnome-shell-extension-dashtodock gnome-shell-extensions gnome-terminal gnome-tweaks \
-		mutter nautilus nautilus-extension-gnome-terminal network-manager-gnome sane-airscan sane-utils seahorse \
+		mutter nautilus nautilus-extension-gnome-terminal network-manager-gnome rclone sane-airscan sane-utils seahorse \
 		software-properties-gtk sudo xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs
 	else
 		DEBIAN_FRONTEND=noninteractive aptitude install -y -R alsa-utils chrome-gnome-shell cups-common \
 		dbus-x11 dconf-editor firefox-esr fuse_ fuse3 gdm3 gjs gkbd-capplet gnome-applets gnome-control-center \
 		gnome-disk-utility gnome-keyring gnome-session gnome-shell-extension-dashtodock gnome-shell-extensions \
 		gnome-shell-extensions-extra gnome-shell-extension-manager gnome-terminal gnome-tweaks mutter nautilus \
-		nautilus-extension-gnome-terminal network-manager-gnome power-profiles-daemon sane-airscan sane-utils \
+		nautilus-extension-gnome-terminal network-manager-gnome power-profiles-daemon rclone sane-airscan sane-utils \
 		seahorse software-properties-gtk sudo xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk \
 		xdg-user-dirs
 	fi
